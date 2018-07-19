767ca7b061d192c9863f481d5bcab8ee6db48b48
ignatandrei <ignatandrei@yahoo.com>
2018-07-19 08:26:13 +0300

    trivia rewriter

--- AOPRoslyn/AOPRoslyn/RewriteCode.cs
+++ AOPRoslyn/AOPRoslyn/RewriteCode.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
 namespace AOPRoslyn
@@ -21,13 +23,31 @@ namespace AOPRoslyn
         public string RewriteCodeMethod()
         {
             var tree = CSharpSyntaxTree.ParseText(Code);
-
+            
             var node = tree.GetRoot();
-
+            node=ModifyRegionToTrivia(node);
             var LG = new MethodRewriter(FormatterFirstLine,FormatterLastLine);
             var sn = LG.Visit(node);
             return sn.NormalizeWhitespace().ToFullString();
 
+        }
+
+        private SyntaxNode ModifyRegionToTrivia(SyntaxNode syntaxNode)
+        {
+            while (true)
+            {
+                var trivia = syntaxNode.DescendantTrivia(null, false)
+                    .Select(it=>(SyntaxTrivia)it)
+                    .Select(it=>new { it= it,Kind=it.Kind() })
+                    .ToArray()
+                    .FirstOrDefault(t => t.Kind == SyntaxKind.RegionDirectiveTrivia || t.Kind == SyntaxKind.EndRegionDirectiveTrivia);
+                if (trivia == null || trivia.Kind == SyntaxKind.None)
+                {
+                    break;
+                }
+                syntaxNode = syntaxNode.ReplaceTrivia(trivia.it, SyntaxFactory.Comment("//was a region " + Environment.NewLine));
+            }
+            return syntaxNode;
         }
     }
 }
---
+++ AOPRoslyn/TestAOP/TestRegion.cs
@@ -0,0 +1,52 @@
+﻿using AOPRoslyn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAOP
+{
+    [TestClass]
+    public class TestRegion
+    {
+        [TestMethod]
+        public void TestOneRegion()
+        {
+
+            var rc = new RewriteCode();
+            rc.Code = @"
+using System;
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#region test
+              var dt=DateTime.Now;
+#endregion
+        }
+     }
+}";
+            
+            var result = rc.RewriteCodeMethod();
+            var newCode = @"
+using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" + "\"Program_Main_6\"" + @");
+            //was a region 
+            var dt = DateTime.Now;
+            
+        }
+    }
+}";
+            Assert.AreEqual(result.Replace(Environment.NewLine, "").Replace(" ",""), newCode.Replace(Environment.NewLine, "").Replace(" ", ""));
+        }
+    }
+}
