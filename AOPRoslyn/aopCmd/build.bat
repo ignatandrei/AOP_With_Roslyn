c43101cf7111aa7fc5625325470644624e67163d
ignatandrei <ignatandrei@yahoo.com>
2018-08-16 06:21:13 +0300

    building recursive this project

--- AOPRoslyn/aopCmd/build.bat
+++ AOPRoslyn/aopCmd/build.bat
@@ -2,3 +2,13 @@ del *.nupkg
 dotnet tool uninstall -g dotnet-aop
 dotnet pack --output ./
 dotnet tool install -g dotnet-aop --add-source ./ --version 2.0.0
+cd ..
+dotnet aop
+cd aopCmd
+del *.nupkg 
+dotnet tool uninstall -g dotnet-aop
+dotnet pack --output ./
+dotnet tool install -g dotnet-aop --add-source ./ --version 2.0.0
+echo now run 
+echo dotnet aop
+echo in whatever folder you have the .cs files
