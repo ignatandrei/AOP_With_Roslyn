a652a11c2046b9cd9d26ab8f4659e786c32abebb
ignatandrei <ignatandrei@yahoo.com>
2018-08-23 07:43:31 +0300

    added code coverage

---
+++ AOPRoslyn/TestAOP/CodeCoverage.bat
@@ -0,0 +1,7 @@
+rem dotnet tool install --global coverlet.console
+rem dotnet tool install -g dotnet-reportgenerator-globaltool --version 4.0.0-alpha10
+rem please be sure to include into csproj  <PackageReference Include="coverlet.msbuild" Version="2.1.0" />
+dotnet test TestAOP.csproj --configuration release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+reportgenerator "-reports:coverage.opencover.xml" "-targetdir:coveragereport" -reporttypes:HTMLInline;HTMLChart
+
+	
