e6d8f9fa14db63d85e783cb865e8ef5d14864694
ignatandrei <ignatandrei@yahoo.com>
2018-08-23 07:20:14 +0300

    adding test for methodmodifier

--- AOPRoslyn/TestAOP/TestRewriteCode.cs
+++ AOPRoslyn/TestAOP/TestRewriteCode.cs
@@ -1,5 +1,6 @@
 ﻿using AOPRoslyn;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,7 +10,45 @@ namespace TestAOP
 {
     [TestClass]
     public class TestRewriteCode
-    {  
+    {
+        [TestMethod]
+        public void TestModifierMethod()
+        {
+            var rc = new RewriteCode();
+            rc.Options.PreserveLinesNumber = false;
+            rc.Formatter.MethodsToLog = ModifierMethod.PublicKeyword;
+            rc.Code = @"
+using System;
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dt=DateTime.Now;            
+            dt=dt;
+        }
+     }
+}";
+            var result = rc.RewriteCodeMethod();
+            var newCode = @"
+using System;
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dt=DateTime.Now;            
+            dt=dt;
+        }
+     }
+}";//same code, no aop
+            result = result.Replace(" ", "").Replace(Environment.NewLine, "");
+            newCode = newCode.Replace(" ", "").Replace(Environment.NewLine, "");
+            result.ShouldBe(newCode);
+        }
+
 
         [TestMethod]
         public void TestAOPFile()
@@ -20,9 +59,9 @@ namespace TestAOP
             rc.Rewrite();
             string expected = File.ReadAllText(fileName);
             Assert.AreNotEqual(text, expected);
-            Assert.AreEqual(File.ReadAllText(fileName+ ".expected"), expected);
+            Assert.AreEqual(File.ReadAllText(fileName + ".expected"), expected);
             FileInfo fi = new FileInfo(fileName);
-            fi.IsReadOnly = false;            
+            fi.IsReadOnly = false;
             File.WriteAllText(fileName, text);
         }
         [TestMethod]
@@ -30,7 +69,7 @@ namespace TestAOP
         {
             string folderName = @"ClassesForTesting";
             int nr = 0;
-            var rc = new RewriteCodeFolder(folderName,"*.cs");
+            var rc = new RewriteCodeFolder(folderName, "*.cs");
             var fileText = new Dictionary<string, string>();
             rc.StartProcessingFile += (object sender, string fileName) =>
             {
@@ -44,9 +83,9 @@ namespace TestAOP
                 File.WriteAllText(fileName, fileText[fileName]);
             };
             rc.Rewrite();
-            Assert.IsTrue(nr > 0,"should process at least one file");
+            Assert.IsTrue(nr > 0, "should process at least one file");
         }
 
-        
+
     }
 }
