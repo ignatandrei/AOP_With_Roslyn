cdecea85a028656b0efbe479739824bc8384e016
ignatandrei <ignatandrei@yahoo.com>
2018-08-30 09:50:14 +0300

    adding statistics

---
+++ AOPRoslyn/AOPStatistics/AOPStatistics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOPStatistics
+{
+    public class AOPStatistics
+    {
+        public static ConcurrentDictionary<Method, long> timingMethod;
+        static AOPStatistics()
+        {
+            timingMethod = new ConcurrentDictionary<Method, long>();
+        }
+        
+        public static void PushMethod(Method m, long timeExecuting)
+        {
+            timingMethod.AddOrUpdate(m, timeExecuting, (newValue, oldValue) => oldValue + timeExecuting);
+        }
+
+        public static KeyValuePair<string,long>[] ProcessingClassTotalTime()
+        {
+            return timingMethod.GroupBy(it => it.Key.className)
+                .Select(it => new KeyValuePair<string, long>(it.Key, it.Sum(a => a.Value)))
+                .ToArray();
+        }
+    }
+}
---
+++ AOPRoslyn/AOPStatistics/AOPStatisticsMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AOPStatistics
+{
+    public struct Method
+    {
+        public string className;
+        public string methodName;
+        public int line;
+    }
+    public class AOPStatisticsMethod : IDisposable
+    {
+        private readonly string methodName;
+        private readonly string className;
+        private readonly int line;
+        private readonly Stopwatch sw;
+        public AOPStatisticsMethod(string className, string methodName, int line)
+        {
+            sw = Stopwatch.StartNew();            
+            this.className = className;
+            this.methodName = methodName;
+            this.line = line;
+        }
+
+        
+        public void Dispose()
+        {
+            sw.Stop();
+            AOPStatistics.PushMethod(new Method() {  className = this.className, line= this.line, methodName= this.methodName}
+                , sw.ElapsedMilliseconds);
+            
+
+        }
+    }
+}
