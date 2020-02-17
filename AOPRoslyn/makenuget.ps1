4b45e4ecd3e445d611475e7979f0d0d142e604cd
ignatandrei <ignatandrei@yahoo.com>
2020-02-17 13:44:35 +0200

    artifacts

--- AOPRoslyn/makenuget.ps1
+++ AOPRoslyn/makenuget.ps1
@@ -1,2 +1,3 @@
-$date = Get-Date -Format "yyyy.MM.dd.HHmmss"
-Write-Host $date
+$version = Get-Date -Format "yyyy.MM.dd.HHmm"
+Write-Host $version
+ dotnet tool run dotnet-property "**/*.csproj" Version:"$version"
