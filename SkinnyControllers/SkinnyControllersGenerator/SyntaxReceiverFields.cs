c10a34dbe118e79c2251917a0fbc0ee8017312bc
ignatandrei <ignatandrei@yahoo.com>
2020-11-23 21:17:34 +0200

    figuring how to recognize

--- SkinnyControllers/SkinnyControllersGenerator/SyntaxReceiverFields.cs
+++ SkinnyControllers/SkinnyControllersGenerator/SyntaxReceiverFields.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SkinnyControllersCommon;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,12 @@ namespace SkinnyControllersGenerator
 {
     public class SyntaxReceiverFields : ISyntaxReceiver
     {
+        string autoActions = typeof(AutoActionsAttribute).Name;
+
+        public SyntaxReceiverFields()
+        {
+
+        }
         public List<FieldDeclarationSyntax> CandidateFields { get; } = new List<FieldDeclarationSyntax>();
 
         public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
@@ -15,8 +22,20 @@ namespace SkinnyControllersGenerator
             if (syntaxNode is FieldDeclarationSyntax fieldDeclarationSyntax
                         && fieldDeclarationSyntax.AttributeLists.Count > 0)
             {
-                //maybe find from here the AutoActionsAttribute
-                CandidateFields.Add(fieldDeclarationSyntax);
+                foreach(var al in fieldDeclarationSyntax.AttributeLists)
+                {
+                    var att = al.Attributes;
+                    foreach(var at in att)
+                    {
+                        var x = at.Name as IdentifierNameSyntax;
+                        if(autoActions.Contains(x.Identifier.Text))
+                        {
+                            CandidateFields.Add(fieldDeclarationSyntax);
+                            return;
+                        }
+                    }
+                }
+                
             }
 
         }
