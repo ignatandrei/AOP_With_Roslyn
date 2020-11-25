93e4ecac889da33af7b51e87c26be18cc3ca5706
ignatandrei <ignatandrei@yahoo.com>
2020-11-25 07:08:07 +0200

    7.01

--- SkinnyControllers/SkinnyControllersGenerator/AutoActionsGenerator.cs
+++ SkinnyControllers/SkinnyControllersGenerator/AutoActionsGenerator.cs
@@ -19,16 +19,16 @@ namespace SkinnyControllersGenerator
         public void Execute(GeneratorExecutionContext context)
         {
             {
-                var dd = new DiagnosticDescriptor("Test", "StartExecution", "StartExecution", "SkinnyControllers", DiagnosticSeverity.Info, true);
+                var dd = new DiagnosticDescriptor("Test", "StartExecution", "StartExecution", "SkinnyControllers", DiagnosticSeverity.Warning, true);
                 var d = Diagnostic.Create(dd, Location.Create("skinnycontrollers.cs", new TextSpan(1, 2), new LinePositionSpan()));
                 context.ReportDiagnostic(d);
             }
-            if (!context.Compilation.ReferencedAssemblyNames.Any(ai => ai.Name.Equals("SkinnyControllersCommon", StringComparison.OrdinalIgnoreCase)))
-            {
-                var dd= new DiagnosticDescriptor("Andrei","do not have skinny controllers common", "do not have skinny controllers common", "SkinnyControllers", DiagnosticSeverity.Error, true);
-                var d = Diagnostic.Create(dd, Location.Create("skinnycontrollers.cs", new TextSpan(1,2),new LinePositionSpan()));
-                context.ReportDiagnostic(d);
-            }
+            //if (!context.Compilation.ReferencedAssemblyNames.Any(ai => ai.Name.Equals("SkinnyControllersCommon", StringComparison.OrdinalIgnoreCase)))
+            //{
+            //    var dd= new DiagnosticDescriptor("Andrei","do not have skinny controllers common", "do not have skinny controllers common", "SkinnyControllers", DiagnosticSeverity.Error, true);
+            //    var d = Diagnostic.Create(dd, Location.Create("skinnycontrollers.cs", new TextSpan(1,2),new LinePositionSpan()));
+            //    context.ReportDiagnostic(d);
+            //}
 
             if (!(context.SyntaxReceiver is SyntaxReceiverFields receiver))
                 return;
