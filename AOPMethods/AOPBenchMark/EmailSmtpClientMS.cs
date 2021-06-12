730f99c2e1cece5d84319f71b9362dc9e6074d1e
ignatandrei <ignatandrei@yahoo.com>
2021-06-12 16:16:41 +0300

    benchmark

--- AOPMethods/AOPBenchMark/EmailSmtpClientMS.cs
+++ AOPMethods/AOPBenchMark/EmailSmtpClientMS.cs
@@ -1,23 +1,37 @@
 ﻿using AOPMethodsCommon;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
 using System.Linq;
 
 namespace AOPBenchMark
 {
+    [SimpleJob(RuntimeMoniker.Net50)]
+    //[SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [RPlotExporter]
     [AutoMethods(template = TemplateMethod.CustomTemplateFile, CustomTemplateFileName = "ClassToDictionary.txt")]
 
     public partial class EmailSmtpClientMS 
     {
+        [Benchmark]
         public string GetHostReflection()
         {
             return this.GetType().GetProperty("Host").GetValue(this).ToString();
         }
+        [Benchmark]
         public string GetHostViaDictionary()
         {
             return this.ReadMyProperties().First(it => it.Key == "Host").Value.ToString();
         }
+        [Benchmark]
         public string GetHostViaSwitch()
         {
             return this.GetValueProperty("Host").ToString();
+        }
+        [GlobalSetup]
+        public void Setup()
+        {
+            Host = "http://msprogrammer.serviciipeweb.ro/";
+
         }
         public EmailSmtpClientMS()
         {
--- AOPMethods/AOPBenchMark/Program.cs
+++ AOPMethods/AOPBenchMark/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using System;
 
 namespace AOPBenchMark
 {
@@ -7,10 +9,15 @@ namespace AOPBenchMark
         static void Main(string[] args)
         {
             var email = new EmailSmtpClientMS();
-            email.Host = "http://msprogrammer.serviciipeweb.ro/";
+            email.Setup();
             Console.WriteLine(email.GetHostReflection());
             Console.WriteLine(email.GetHostViaDictionary());
             Console.WriteLine(email.GetHostViaSwitch());
+            BenchmarkRunner.Run<EmailSmtpClientMS>(
+                ManualConfig
+                    .Create(DefaultConfig.Instance)
+                    .WithOption(ConfigOptions.DisableOptimizationsValidator,true)
+                    );
 
         }
     }
